56e4604c7f4805812cdd1beb9214204e90112b15
shun-hikari <unoshun19890325@icloud.com>
2025-08-14 21:59:16 +0900

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,16 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+const root = document.getElementById('root')
+if (!root) throw new Error('Missing #root element')
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
